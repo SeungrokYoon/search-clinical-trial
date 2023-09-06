feb01417a7941941c17616f4caf87040b2c889d5
Seungrok <sonicrok93@gmail.com>
2023-09-06 14:07:00 +0900

    feat: setup default router & pages

---
+++ src/pages/RootPage.tsx
@@ -0,0 +1,12 @@
+import Search from '../components/Search/Search'
+
+function RootPage() {
+  return (
+    <div>
+      <h1>RootPage</h1>
+      <Search />
+    </div>
+  )
+}
+
+export default RootPage
