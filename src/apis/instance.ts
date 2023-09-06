b9b67410f42ee21b48a2268ea3acff3e53a86598
Seungrok <sonicrok93@gmail.com>
2023-09-07 00:41:59 +0900

    chore: remove unused comments

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,18 +3,13 @@ import axios from 'axios'
 const axiosInstance = axios.create({ baseURL: 'http://localhost:4000/' })
 
 axiosInstance.interceptors.request.use((config) => {
-  /** @description API 호출될 때마다 console.info 출력*/
-  console.info('API CALL to ', config.url, config.params)
+  console.info('calling api')
   return config
 })
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    /** @description 200대 응답이라면 이 콜백이 실행됨*/
-    return response
-  },
+  (response) => response,
   (error) => {
-    /** @description 200대 응답이 아니라면 이 콜백이 실행됨*/
     console.error(error)
     return Promise.reject(error)
   }
