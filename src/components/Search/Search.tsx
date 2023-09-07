c8a94f52343d4a4554329a8bb8b729c6e8d9531b
Seungrok <sonicrok93@gmail.com>
2023-09-07 12:38:34 +0900

    feat: search & display result in Search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,26 +4,33 @@ import { selectSearch, setSearchTerm } from './searchSlice'
 import useDebounce from '../../hooks/useDebounce'
 import useSuggestion from '../../hooks/useSuggestion'
 import { useAppDispatch, useAppSelector } from '../../store/reduxHooks'
+import AsyncButton from '../AsyncButton/AsyncButton'
 
 function Search() {
+  const { data, fetchData, isLoading, isError } = useSuggestion('')
   return (
     <div>
-      <SearchInput />
-      <SearchResultList />
+      <SearchInput error={isError} loading={isLoading} onSearch={fetchData} />
+      <SearchResultList data={data} error={isError} loading={isLoading} />
     </div>
   )
 }
 
 export default Search
 
-function SearchInput() {
+interface SearchInputProps {
+  loading: boolean
+  error: boolean
+  onSearch: (searchTerm: string) => Promise<void>
+}
+
+function SearchInput({ loading, error, onSearch }: SearchInputProps) {
   const search = useAppSelector(selectSearch)
   const dispatch = useAppDispatch()
-  const { data, fetchData } = useSuggestion('')
 
   const handleSearch = useDebounce(
     (term: string) => {
-      fetchData(term)
+      onSearch(term)
     },
     500,
     []
@@ -36,21 +43,39 @@ function SearchInput() {
   }
 
   return (
-    <>
-      <input value={search.searchTerm} onChange={handleChange} />
-      <ul>
-        {data.map((v) => (
-          <li key={v.sickCd}>{v.sickNm}</li>
-        ))}
-      </ul>
-    </>
+    <label htmlFor="searchInput">
+      <input type="text" value={search.searchTerm} onChange={handleChange} />
+      <AsyncButton
+        error={error}
+        loading={loading}
+        type="button"
+        onClick={() => {
+          onSearch(search.searchTerm)
+        }}
+      >
+        검색
+      </AsyncButton>
+    </label>
   )
 }
 
-function SearchResultList() {
+interface SearchResultListProps {
+  data: any[]
+  loading: boolean
+  error: boolean
+}
+
+function SearchResultList({ data }: SearchResultListProps) {
   return (
-    <ul>
-      <li>result</li>
-    </ul>
+    <aside>
+      <ul>
+        {data.length === 0 && <p>검색어 없음</p>}
+        {data.map((v) => (
+          <li key={v.sickCd} tabIndex={0}>
+            {v.sickNm}
+          </li>
+        ))}
+      </ul>
+    </aside>
   )
 }
