153e08821280a4670df79116a04e33d402036dd2
Seungrok <sonicrok93@gmail.com>
2023-09-08 23:03:16 +0900

    feat: implement expire cache

--- src/provider/CacheContextProvider.tsx
+++ src/provider/CacheContextProvider.tsx
@@ -3,7 +3,9 @@ import { PropsWithChildren } from 'react'
 import { CacheContext } from '../context/cacheContext'
 import CacheClient from '../store/cacheClient'
 
+const EXPIRE_MINUTE = 1
+
 export function CacheContextProvider({ children }: PropsWithChildren) {
-  const client = new CacheClient()
+  const client = new CacheClient(EXPIRE_MINUTE)
   return <CacheContext.Provider value={client}>{children} </CacheContext.Provider>
 }
--- src/store/cacheClient.ts
+++ src/store/cacheClient.ts
@@ -1,16 +1,31 @@
 import { CacheStore } from './cacheStore'
 
+interface QueryData<T> {
+  data: T[]
+  expireTime: number
+}
+
 class CacheClient {
   #cache
-  constructor() {
+  #expireDelta
+  constructor(expireMinute: number) {
     this.#cache = CacheStore
+    this.#expireDelta = expireMinute * 60000
   }
 
-  set<T>(queryKey: string, data: T) {
-    !this.#cache.has(queryKey) && this.#cache.set(queryKey, data)
+  set<T>(queryKey: string, data: T[]) {
+    const requestTime = Date.now()
+    const queryData: QueryData<T> = { data, expireTime: requestTime + this.#expireDelta }
+    this.#cache.set(queryKey, queryData)
   }
+
   get(queryKey: string) {
-    return this.#cache.get(queryKey)
+    const requestTime = Date.now()
+    if (this.#cache.has(queryKey)) {
+      const expired = this.#cache.get(queryKey).expireTime < requestTime
+      return expired ? null : this.#cache.get(queryKey).data
+    }
+    return null
   }
 }
 
